126f6f25bd4d8d3d1ac12b83b6e8985d82c63e1f
Pratham Loya <prathamloya12345@gmail.com>
2024-10-13 23:57:16 +0530

    modified notebook

---
+++ Files/JAVA/hello.java
@@ -0,0 +1,17 @@
+
+/**
+ * hello
+ */
+public class hello {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        while (i < 5) {
+            System.out.println("Hello");
+            i++;
+        }
+
+    }
+}
